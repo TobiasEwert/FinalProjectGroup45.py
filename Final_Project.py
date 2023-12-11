e675b35b61505777f360b91afd7e372de70c8762
localignitro <150964513+localignitro@users.noreply.github.com>
2023-12-10 19:22:36 -0500

    added the convert_to_wav function and file type exception handling.

--- Final_Project.py
+++ Final_Project.py
@@ -6,10 +6,11 @@ import numpy as np
 from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
+from os import path
+from pydub import utils, AudioSegment
+from pydub.playback import play
 
-from pydub import AudioSegment
 output = "result.wav"
-gfile = ''
 # create the root window
 root = tk.Tk()
 root.title('Tkinter Open File Dialog')
@@ -22,9 +23,21 @@ Create an Open dialog and
 return the selected filename(s) that correspond to 
 existing file(s).
 '''
+def convert_to_wav(filename):
+    main, extension = path.splitext(filename)
+    if extension != ".wav":
+        extension = ".wav"
+        sound = AudioSegment.from_file(filename)
+        sound.export(main + extension, format="wav")
+        gfile = main + extension
+    else:
+        gfile = filename
+    return gfile
 def select_file():
     filetypes = (
         ('Audio files', ".wav"),
+        ('Audio files' , ".m4a"),
+        ('Audio files', ".mp3"),
         ('All files', '*.*')
     )
 
@@ -32,33 +45,37 @@ def select_file():
         title='Open a file',
         initialdir='/',
         filetypes=filetypes)
-    gfile = filename
-
-    # tkinter.messagebox — Tkinter message prompts
-    showinfo(
-        title='Selected File',
-        message=filename
-    )
-
-    gfile_label = ttk.Label(root, text=gfile)
-    gfile_label.pack(side="bottom")
-
-    with contextlib.closing(wave.open(gfile, 'r')) as f:
-        print("num channels: ", f.getnchannels())
-        frames = f.getnframes()
-        rate = f.getframerate()
-        duration = frames / float(rate)
-        print(duration)
-        sig = np.frombuffer(f.readframes(16000), dtype=np.int16)
-        sig = sig[:]
-
-        plt.figure(1)
-        plt.title("Clap")
-        plt.plot(sig)
-        plt.show()
+    try:
+        gfile = convert_to_wav(filename)
+        # tkinter.messagebox — Tkinter message prompts
+        showinfo(
+            title='Selected File',
+            message=filename
+        )
 
 
+        gfile_label = ttk.Label(root, text=filename)
+        gfile_label.pack(side="bottom")
 
+        with contextlib.closing(wave.open(gfile, 'r')) as f:
+            print("num channels: ", f.getnchannels())
+            frames = f.getnframes()
+            rate = f.getframerate()
+            duration = frames / float(rate)
+            print(duration)
+            sig = np.frombuffer(f.readframes(16000), dtype=np.int16)
+            sig = sig[:]
+
+            plt.figure(1)
+            plt.title("Clap")
+            plt.plot(sig)
+            plt.show()
+
+    except:
+        showinfo(
+            title='Filetype not supported',
+            message= 'Please select a supported audio file type.'
+        )
 # open button
 open_button = ttk.Button(
     root,
